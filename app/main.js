2b44eda9045e2cb3bfacdaeb10de83ae5a892b31
Lagacy_code <miraglioenzo93@gmail.com>
2025-04-17 15:06:05 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,10 +1,14 @@
 const net = require("net");
-
+console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
+    server.close();
+  });
+  socket.on("data", (data) => {
+    console.log(data);
+    const path = data.toString().split(" ")[1];
+    const responseStatus = path === "/" ? "200 ok" : "404 NOT Found";
+    socket.write(`HTTP/1.1 ${responseStatus} \r\n\r\n`);
   });
 });
-
-server.listen(4221, "localhost");
