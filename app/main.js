5ff21cf146a19b2b0678db52e49f21901e368936
Lagacy_code <miraglioenzo93@gmail.com>
2025-04-17 15:12:23 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,15 +1,16 @@
 const net = require("net");
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
     server.close();
   });
   socket.on("data", (data) => {
+    console.log(data);
     const path = data.toString().split(" ")[1];
-    const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
-    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+    const responseStatus = path === "/" ? "200 ok" : "404 NOT Found";
+    socket.write(`HTTP/1.1 ${responseStatus} \r\n\r\n`);
   });
 });
+
+server.listen(4221, "localhost");
