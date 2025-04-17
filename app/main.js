3ee23795d85583af0fd570f158a2fc1ae90d0a51
Lagacy_code <miraglioenzo93@gmail.com>
2025-04-17 16:36:04 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -7,16 +7,18 @@ const server = net.createServer((socket) => {
   });
 
   socket.on("data", (data) => {
+    console.log(data.toString());
     const initialPath = data.toString().split(" ")[1];
+    const path = initialPath.includes("echo") ? initialPath.split("/")[2] : initialPath;
+
     const acceptedPaths = [
       { path: "/echo", dynamic: true },
       { path: "/user-agent", dynamic: false },
       { path: "/", dynamic: false },
+      { path: "/files", dynamic: true },
     ];
 
     const userAgent = initialPath === "/user-agent" ? data.toString().split("User-Agent: ", data.toString().length)[1].trim() : undefined;
-
-    const path = initialPath.includes("echo") ? initialPath.split("/")[2] : initialPath;
 
     const response = (id = 0) => {
       if (id >= acceptedPaths.length) return "404 Not Found";
