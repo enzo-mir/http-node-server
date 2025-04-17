c2ba7adc955dd27c3c06782d4e4311fc4ba27c9f
Lagacy_code <miraglioenzo93@gmail.com>
2025-04-17 16:09:00 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
@@ -7,12 +8,13 @@ const server = net.createServer((socket) => {
 
   socket.on("data", (data) => {
     const initialPath = data.toString().split(" ")[1];
+    const acceptedPaths = ["/echo", "/user-agent", "/"];
 
     const userAgent = initialPath == "/user-agent" ? data.toString().split("User-Agent: ", data.toString().length)[1].trim() : undefined;
 
     const path = initialPath.includes("echo") ? initialPath.split("/")[2] : initialPath;
 
-    const response = "200 OK";
+    const response = acceptedPaths.includes(initialPath) ? "200 OK" : "404 Not Found";
 
     socket.write(
       `HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent ? userAgent.length : path.length}\r\n\r\n${userAgent || path}`
