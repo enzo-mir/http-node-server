5227d553fad875ea96b85493af0fd278264441ff
Lagacy_code <miraglioenzo93@gmail.com>
2025-04-17 16:15:03 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -15,12 +15,12 @@ const server = net.createServer((socket) => {
     const path = initialPath.includes("echo") ? initialPath.split("/")[2] : initialPath;
 
     const response = acceptedPaths.map((v) => {
-      if (initialPath.includes(v)) {
+      if (initialPath.startsWith(v)) {
         return "200 OK";
       } else {
         return "404 Not Found";
       }
-    });
+    })[0];
 
     socket.write(
       `HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent ? userAgent.length : path.length}\r\n\r\n${userAgent || path}`
