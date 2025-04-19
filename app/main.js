5d93c55b5965957f645576fcd35dd142787d800b
Lagacy_code <miraglioenzo93@gmail.com>
2025-04-19 10:00:07 +0200

    [any message]

--- app/main.js
+++ app/main.js
@@ -38,58 +38,61 @@ const getAcceptContent = (req) => {
 const server = createServer((socket) => {
   socket.on("data", async (data) => {
     const req = data.toString();
-    const path = req.split(" ")[1];
     const headers = req.split("\r\n").reduce((acc, line) => {
       const [key, value] = line.split(": ");
       if (key && value) acc[key] = value;
       return acc;
     }, {});
-    if (path === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    else if (path.startsWith("/files/")) {
-      const directory = process.argv[3];
-      const filename = path.split("/files/")[1];
-      if (req.includes("POST")) {
-        const content = getBody(req);
-        const res = await postFileRequest(filename, content);
-      }
-      if (existsSync(`${directory}/${filename}`)) {
-        const content = readFileSync(`${directory}/${filename}`).toString();
-        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
-        socket.write(res);
+    const path = req.split(" ")[1];
+    while (req.includes("\r\n\r\n")) {
+      if (path === "/") {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      } else if (path.startsWith("/files/")) {
+        const directory = process.argv[3];
+        const filename = path.split("/files/")[1];
+        if (req.includes("POST")) {
+          const content = getBody(req);
+          const res = await postFileRequest(filename, content);
+        }
+        if (existsSync(`${directory}/${filename}`)) {
+          const content = readFileSync(`${directory}/${filename}`).toString();
+          const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+          socket.write(res);
+        } else {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+      } else if (path === "/user-agent") {
+        req.split("\r\n").map((line) => {
+          if (line.includes("User-Agent")) {
+            const res = line.split(" ")[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n`);
+          }
+        });
+      } else if (path.startsWith("/echo/")) {
+        let body = path.split("/echo/")[1];
+        const contentEncoding = getAcceptContent(req);
+
+        if (contentEncoding) {
+          const content = gzipSync(body);
+          const data = new Buffer.from(content);
+          body = contentEncoding.includes("gzip") ? data : body;
+
+          socket.write(
+            `HTTP/1.1 200 OK\r\n${contentEncoding ? "Content-Encoding: " + contentEncoding : ""}\r\nContent-Type: text/plain\r\nContent-Length: ${
+              body.length
+            }\r\n\r\n`
+          );
+          socket.write(data);
+        }
+
+        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}\r\n\r`);
       } else {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       }
-    } else if (path === "/user-agent") {
-      req.split("\r\n").map((line) => {
-        if (line.includes("User-Agent")) {
-          const res = line.split(" ")[1];
-          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n`);
-        }
-      });
-    } else if (path.startsWith("/echo/")) {
-      let body = path.split("/echo/")[1];
-      const contentEncoding = getAcceptContent(req);
 
-      if (contentEncoding) {
-        const content = gzipSync(body);
-        const data = new Buffer.from(content);
-        body = contentEncoding.includes("gzip") ? data : body;
-
-        socket.write(
-          `HTTP/1.1 200 OK\r\n${contentEncoding ? "Content-Encoding: " + contentEncoding : ""}\r\nContent-Type: text/plain\r\nContent-Length: ${
-            body.length
-          }\r\n\r\n`
-        );
-        socket.write(data);
+      if (headers["Connection"] && headers["Connection"].toLowerCase() === "close") {
+        socket.end();
       }
-
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}\r\n\r`);
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-    }
-
-    if (headers["Connection"] && headers["Connection"].toLowerCase() === "close") {
-      socket.end();
     }
   });
 
